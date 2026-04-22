124b68c0dd003ac41d62ef14e40895550f5edc78
hacker444-coder <ronishtamang631@gmail.com>
2026-04-22 15:33:31 +0545

    change link in verifying cuz of dangerous site notice

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -60,7 +60,7 @@ const email = req.body.email.trim().toLowerCase();
       isVerified: false,
     });
 
-    const verifyLink = `${process.env.BASE_URL}/api/auth/user/verify-email?token=${verificationToken}`;
+    const verifyLink = `${process.env.FRONTEND_URL}/api/auth/user/verify-email?token=${verificationToken}`;
 
     await sendEmail(
       email,
