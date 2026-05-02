9e384aae0cbf6e8f875abbcc54b23cd896688975
hacker444-coder <ronishtamang631@gmail.com>
2026-05-02 16:05:35 +0545

    fixed some issue in service and conroller

--- Backend/src/modules/cart/routes/cart.routes.js
+++ Backend/src/modules/cart/routes/cart.routes.js
@@ -1,17 +1,13 @@
 import express from "express";
 import authMiddleware from "../../../middlewares/auth.middleware.js";
-import {
-  addToCart,
-  getCart,
-  removeFromCart,
-} from "../controllers/cart.controller.js";
+import cartController from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
-router.post("/add", authMiddleware.userId, addToCart);
+router.post("/add", authMiddleware.userId, cartController.addToCart);
 
-router.get("/", authMiddleware.userId, getCart);
+router.get("/", authMiddleware.userId, cartController.getCart);
 
-router.delete("/remove", authMiddleware.userId, removeFromCart);
+router.delete("/remove", authMiddleware.userId, cartController.removeFromCart);
 
 export default router;
